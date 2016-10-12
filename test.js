992f11755eadac74e448ca795c228dc2601c27f1
Alexey Litvinov <sullenor@yandex-team.ru>
2016-10-12 11:52:32 +0300

    deps update

--- test.js
+++ test.js
@@ -1,21 +1,27 @@
-describe('generic-names', function () {
-  'use strict';
+'use strict';
 
-  var assert = require('assert');
-  var genericNames = require('.');
+const genericNames = require('./index');
+const test = require('tape');
 
-  it('should use cwd if no context provided', function () {
-    var generate = genericNames('[name]__[local]___[hash:base64:5]');
-    assert.equal(generate('foo', '/test/case/source.css'), 'source__foo___2e670');
-  });
+const pattern = '[name]__[local]___[hash:base64:5]';
 
-  it('should generate another hash for the provided context', function () {
-    var generate = genericNames('[name]__[local]___[hash:base64:5]', {context: '/test'});
-    assert.equal(generate('foo', '/test/case/source.css'), 'source__foo___19xFw');
-  });
+test('use `cwd` if no context was provided', t => {
+  const generate = genericNames(pattern);
 
-  it('should generate another hash for the provided hashPrefix', function () {
-    var generate = genericNames('[name]__[local]___[hash:base64:5]', {context: '/test', hashPrefix: '--'});
-    assert.equal(generate('foo', '/test/case/source.css'), 'source__foo___3T0Un');
-  });
+  t.equal(generate('foo', '/test/case/source.css'), 'source__foo___2e670');
+  t.end();
+});
+
+test('generate distinct hash for the provided context', t => {
+  const generate = genericNames(pattern, {context: '/test'});
+
+  t.equal(generate('foo', '/test/case/source.css'), 'source__foo___19xFw');
+  t.end();
+});
+
+test('generate distinct hash for the provided hashPrefix', t => {
+  const generate = genericNames(pattern, {context: '/test', hashPrefix: '--'});
+
+  t.equal(generate('foo', '/test/case/source.css'), 'source__foo___3T0Un');
+  t.end();
 });
